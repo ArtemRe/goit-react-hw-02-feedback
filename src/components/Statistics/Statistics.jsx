bb839fe905b3a6a0e9b0ae2b3b82b8c0f67d40d7
ArtemRe <97356745+ArtemRe@users.noreply.github.com>
2022-09-28 02:33:55 +0300

    Initial commit

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import css from 'components/Statistics/Statistics.module.css';
+
+function randomColor() {
+  let color = `#${(Math.random().toString(16) + '000000')
+    .substring(2, 8)
+    .toUpperCase()}80`;
+  return color;
+}
+export const Statistics = ({ title, stats }) => (
+  <section className={css.statistics}>
+    <h2 className={css.title}>{title}</h2>
+
+    <ul className={css.statList}>
+      {stats.map(stat => {
+        return (
+          <li key={stat.id} style={{ backgroundColor: randomColor() }}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.quantity}>{stat.percentage}%</span>
+          </li>
+        );
+      })}
+    </ul>
+  </section>
+);
+
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+};
---
+++ src/components/frends/Friend.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import css from '../../../src/components/frends/Friend.module.css';
+
+export const Friend  = ({ avatar, name, isOnline }) => {
+  const status = isOnline ? css.statusRed : css.statusGreen;
+  return (
+    <li className={css.item}>
+      <span className={status}>{isOnline}</span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+
+
+Friend.prototype = {
+  id: PropTypes.number.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+
+
+
+
+
+
